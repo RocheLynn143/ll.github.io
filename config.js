11b6af2b27268788b38616c1780b851317adbb4f
Myosotis0096 <1178122463@qq.com>
2024-08-09 21:41:15 +0800

    ll

---
+++ config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+    titleWeb: "乞巧节快乐~~o(*≧▽≦)ツ ",
+    introTitle: '小铃(っ´Ι`)っ',
+    introDesc: `....   9   დ   11   12   13
+    14   15   16   17   18   19   20   ......`,
+    btnIntro: '^^HiHi^^',
+    title: 'Your beauty is like a whisper of the stars in the night 🥰',
+    desc: 'In your eyes, I see the tenderness of the ocean and the brilliance of the stars, like an endless love song  ',
+    btnYes: '点这个~(o゜▽゜)o☆[BINGO!]',
+    btnNo: '不许点这个o(≧口≦)o ',
+    question: '嘿嘿~n(*≧▽≦*)n ，很高兴在茫茫人海中认识你~~',
+    btnReply: 'Yes ~♪ヽ(*＾∇＾*)ﾉ*:･’ﾟ☆',
+    reply: 'Every glance at you feels like stepping into a poetic dream',
+    mess: '倾国倾城 🥰 非花非雾 😘 春风十里独步😘 ',
+    messDesc: '(o゜▽゜)o☆花会沿路盛开，你以后的路也是(o゜▽゜)o☆',
+    btnAccept: 'okiiii~ <3',
+    // messLink: 'https://github.com/zukahai/Confess-Crush' 
+}
